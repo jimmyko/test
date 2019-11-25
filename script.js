d3bdfb9ea8505028a374ef31b72192fced859b78
Jimmy Ko <egkcyaaa@gmail.com>
2019-11-25 00:03:04 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+// -- in page multi step form.
+var multiStepForm = {
+  _init: function() {
+    this.backBtn = null;
+    this.continueBtn = null;
+    this.previousBtn = null;
+    this.nextBtn = null;
+    this.forms = [];
+    this.current;
+  },
+  addForm: function(elem) {
+    this.forms.push(elem);
+  },
+  start: function() {
+    // do nothing when no forms defined.
+    if (this.forms.length == 0) {
+      return;
+    }
+
+    this._init();
+
+    this.current = 0;
+
+    this.processButtons();
+  },
+  hideElem: function(elem) {
+    elem.hide();
+  },
+  showElem: function(elem) {
+    elem.show();
+  },
+  processButtons: function() {
+    if (current == 0) {
+      this.hideElem(this.previousBtn);
+      this.showElem(this.backBtn);
+    }
+    if (current > 0) {
+      this.showElem(this.previousBtn);
+      this.hideelem(this.backBtn);
+    }
+    if (current == this.forms.length - 1) {
+      this.hideElem(this.nextBtn);
+      this.showElem(this.continueBtn);
+    }
+    if (current < this.forms.length - 1) {
+      this.showElem(this.nextBtn);
+      this.hideElem(this.continueBtn);
+    }
+  }
+}
